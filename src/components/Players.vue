daff327d19a83951f35ba8b0097939dc76aa9096
higorasilverio <higorasilverio@outlook.com>
2021-01-28 19:40:10 -0300

    Random generation of teams implemented

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -10,8 +10,8 @@
           id="checkbox-1"
           v-model="status"
           name="checkbox-1"
-          value="accepted"
-          unchecked-value="not_accepted"
+          value='accepted'
+          unchecked-value='not_accepted'
         >
           Select teams at random
         </b-form-checkbox>
@@ -20,37 +20,77 @@
         <div class="container-row shadow-border">
           <div class="team">WHITE TEAM</div>
           <div class="container-column">
-            <div class="spacing"><b-form-input class="alignment" v-model="white.player1" size="sm" placeholder="Player 1"></b-form-input></div>
-            <div class="spacing"><b-form-input class="alignment" v-model="white.player2" size="sm" placeholder="Player 2"></b-form-input></div>
-            <div v-if="players > 4" class="spacing"><b-form-input class="alignment" v-model="white.player3" size="sm" placeholder="Player 3"></b-form-input></div>
-            <div v-if="players > 6" class="spacing"><b-form-input class="alignment" v-model="white.player4" size="sm" placeholder="Player 4"></b-form-input></div>
-            <div v-if="players > 8" class="spacing"><b-form-input class="alignment" v-model="white.player5" size="sm" placeholder="Player 5"></b-form-input></div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="white[0]" size="sm" placeholder="Player 1"></b-form-input>
+            </div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="white[1]" size="sm" placeholder="Player 2"></b-form-input>
+            </div>
+            <div v-if="players > 4" class="spacing">
+              <b-form-input class="alignment" v-model="white[2]" size="sm" placeholder="Player 3"></b-form-input>
+            </div>
+            <div v-if="players > 6" class="spacing">
+              <b-form-input class="alignment" v-model="white[3]" size="sm" placeholder="Player 4"></b-form-input>
+            </div>
+            <div v-if="players > 8" class="spacing">
+              <b-form-input class="alignment" v-model="white[4]" size="sm" placeholder="Player 5"></b-form-input>
+            </div>
           </div>
         </div>
         <div class="container-row shadow-border dark">
           <div class="team">BLACK TEAM</div>
           <div class="container-column">
-            <div class="spacing"><b-form-input class="alignment" v-model="black.player1" size="sm" placeholder="Player 1"></b-form-input></div>
-            <div class="spacing"><b-form-input class="alignment" v-model="black.player2" size="sm" placeholder="Player 2"></b-form-input></div>
-            <div v-if="players > 5" class="spacing"><b-form-input class="alignment" v-model="black.player3" size="sm" placeholder="Player 3"></b-form-input></div>
-            <div v-if="players > 7" class="spacing"><b-form-input class="alignment" v-model="black.player4" size="sm" placeholder="Player 4"></b-form-input></div>
-            <div v-if="players > 9" class="spacing"><b-form-input class="alignment" v-model="black.player5" size="sm" placeholder="Player 5"></b-form-input></div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="black[0]" size="sm" placeholder="Player 1"></b-form-input>
+            </div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="black[1]" size="sm" placeholder="Player 2"></b-form-input>
+            </div>
+            <div v-if="players > 5" class="spacing">
+              <b-form-input class="alignment" v-model="black[2]" size="sm" placeholder="Player 3"></b-form-input>
+            </div>
+            <div v-if="players > 7" class="spacing">
+              <b-form-input class="alignment" v-model="black[3]" size="sm" placeholder="Player 4"></b-form-input>
+            </div>
+            <div v-if="players > 9" class="spacing">
+              <b-form-input class="alignment" v-model="black[4]" size="sm" placeholder="Player 5"></b-form-input>
+            </div>
           </div>
         </div>
       </div>
-      <div v-if="status == 'accepted'">
+      <div v-else>
         <div class="container-row">
           <div class="container-column">
-            <div class="spacing"><b-form-input class="alignment" v-model="random.player1" size="sm" placeholder="Player 1"></b-form-input></div>
-            <div class="spacing"><b-form-input class="alignment" v-model="random.player2" size="sm" placeholder="Player 2"></b-form-input></div>
-            <div class="spacing"><b-form-input class="alignment" v-model="random.player3" size="sm" placeholder="Player 3"></b-form-input></div>
-            <div class="spacing"><b-form-input class="alignment" v-model="random.player4" size="sm" placeholder="Player 4"></b-form-input></div>
-            <div v-if="players > 4" class="spacing"><b-form-input class="alignment" v-model="random.player5" size="sm" placeholder="Player 5"></b-form-input></div>
-            <div v-if="players > 5" class="spacing"><b-form-input class="alignment" v-model="random.player6" size="sm" placeholder="Player 6"></b-form-input></div>
-            <div v-if="players > 6" class="spacing"><b-form-input class="alignment" v-model="random.player7" size="sm" placeholder="Player 7"></b-form-input></div>
-            <div v-if="players > 7" class="spacing"><b-form-input class="alignment" v-model="random.player8" size="sm" placeholder="Player 8"></b-form-input></div>
-            <div v-if="players > 8" class="spacing"><b-form-input class="alignment" v-model="random.player9" size="sm" placeholder="Player 9"></b-form-input></div>
-            <div v-if="players > 9" class="spacing"><b-form-input class="alignment" v-model="random.player10" size="sm" placeholder="Player 10"></b-form-input></div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="random[0]" size="sm" placeholder="Player 1"></b-form-input>
+            </div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="random[1]" size="sm" placeholder="Player 2"></b-form-input>
+            </div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="random[2]" size="sm" placeholder="Player 3"></b-form-input>
+            </div>
+            <div class="spacing">
+              <b-form-input class="alignment" v-model="random[3]" size="sm" placeholder="Player 4"></b-form-input>
+            </div>
+            <div v-if="players > 4" class="spacing">
+              <b-form-input class="alignment" v-model="random[4]" size="sm" placeholder="Player 5"></b-form-input>
+            </div>
+            <div v-if="players > 5" class="spacing">
+              <b-form-input class="alignment" v-model="random[5]" size="sm" placeholder="Player 6"></b-form-input>
+            </div>
+            <div v-if="players > 6" class="spacing">
+              <b-form-input class="alignment" v-model="random[6]" size="sm" placeholder="Player 7"></b-form-input>
+            </div>
+            <div v-if="players > 7" class="spacing">
+              <b-form-input class="alignment" v-model="random[7]" size="sm" placeholder="Player 8"></b-form-input>
+            </div>
+            <div v-if="players > 8" class="spacing">
+              <b-form-input class="alignment" v-model="random[8]" size="sm" placeholder="Player 9"></b-form-input>
+            </div>
+            <div v-if="players > 9" class="spacing">
+              <b-form-input class="alignment" v-model="random[9]" size="sm" placeholder="Player 10"></b-form-input>
+            </div>
           </div>
         </div>
       </div>
@@ -68,49 +108,54 @@ export default {
     return {
       players: 6,
       status: 'not_accepted',
-      white: {
-        player1: '',
-        player2: '',
-        player3: '',
-        player4: '',
-        player5: ''
-      },
-      black: {
-        player1: '',
-        player2: '',
-        player3: '',
-        player4: '',
-        player5: ''
-      },
-      random: {
-        player1: '',
-        player2: '',
-        player3: '',
-        player4: '',
-        player5: '',
-        player6: '',
-        player7: '',
-        player8: '',
-        player9: '',
-        player10: ''
-      }
+      randomControl: false,
+      white: ['', '', '', '', '', ''],
+      black: ['', '', '', '', '', ''],
+      random: ['', '', '', '', '', '', '', '', '', '']
     }
   },
   methods: {
-    checkPlayers (append = false) {
-      if (status === 'accepted') {
-        this.$bvToast.toast(`Toast accept`, {
-          title: 'Toast accept',
-          autoHideDelay: 5000,
-          appendToast: append
-        })
+    checkPlayers () {
+      this.randomControl = this.status === 'accepted'
+      if (this.randomControl) {
+        this.selectTeam()
       } else {
-        this.$bvToast.toast(`Toast not_accept`, {
-          title: 'Toast not_accept',
-          autoHideDelay: 5000,
-          appendToast: append
-        })
+        console.log('Not random!')
       }
+    },
+    selectTeam () {
+      this.white = []
+      this.black = []
+      let sequence = this.generateSequence()
+      for (let i = 0; i < this.players; i++) {
+        if (i % 2 === 0) {
+          this.white.push(this.random[sequence[i]])
+        } else {
+          this.black.push(this.random[sequence[i]])
+        }
+      }
+      console.log('White: ' + this.white)
+      console.log('Black: ' + this.black)
+    },
+    generateSequence () {
+      let sequence = []
+      for (let i = 0; i < this.players; i++) {
+        if (this.random[i] === '') {
+          console.log('Still missing players names!')
+          break
+        }
+        let randomNumber = this.getRandomIntInclusive(0, this.players - 1)
+        while (sequence.includes(randomNumber)) {
+          randomNumber = this.getRandomIntInclusive(0, this.players - 1)
+        }
+        sequence.push(randomNumber)
+      }
+      return sequence
+    },
+    getRandomIntInclusive (min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min
     },
     goToWords () {
       this.$router.push('/words')
